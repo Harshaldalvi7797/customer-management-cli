a90f008ef9781733c9a93ed51e4d2ada33a1215e
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2020-11-15 12:10:37 +0530

    commit : define customer Schema

---
+++ Models/customer.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+//Customer Schema
+
+const customerSchema = new mongoose.Schema({
+  firstName: { type: String },
+  lastName: { type: String },
+  email: { type: String },
+  phone_no: { type: String }
+});
+
+//Define and export
+
+module.exports = mongoose.model("customer", customerSchema);
