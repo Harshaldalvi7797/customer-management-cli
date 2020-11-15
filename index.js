c654d913909504586eb28d88f669a8aa1139f7c8
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2020-11-15 12:06:05 +0530

    commit : connect to database using mongoose

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const mongoose = require("mongoose");
+
+//mapp global promise  - get rid of warning
+mongoose.Promise = global.Promise;
+
+//connect to db
+const db = mongoose.connect("mongodb:localhost:27017/customercli", {
+  useMongoClient: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
